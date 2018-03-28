ce6528c27cfbd86e53b01fb663759df7efba9557
William Jattin <wjattin@miami.edu>
2018-03-28 08:06:55 -0400

    codepen updates

--- examples/assignment_4/js/script.js
+++ examples/assignment_4/js/script.js
@@ -1,6 +1,6 @@
 $(function(){
   //Hide stuff
-  $('#getStarted').hide();
+  $('#getStarted, #finish, #previous, .hide').hide();
   //alert("hello");
   $('#title').on('mouseover',function(){
     $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
@@ -9,6 +9,15 @@ $(function(){
 
   $('#title').on('mouseleave',function(){
     $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+  });
+  // show our slides when clicked.
+  $('#getStarted').on('click', function(){
+    $('.hide').show();
+    $('#getStarted').hide();
+  });
+  $('#finish').on('click',function(){
+    $('.hide, #next, #previous, #finish').hide();
+    // to do more....
   });
 
   $('#next').on('click',function(){
@@ -30,6 +39,52 @@ $(function(){
 
     nextItem.toggleClass('active');
 
+    // Conditional eval.
+    if($('li').last().hasClass('active')) {
+      $('#next').hide();
+      $('#finish').show();
+    } else {
+      $('#next').show();
+    }
+
+    // Conditional eval.
+    if($('li').first().hasClass('active')) {
+      $('#previous').hide();
+    } else {
+      $('#previous').show();
+
+    }
+
+
+  });
+
+  $('#previous').on('click',function(){
+    //First, we need to keep track of the current slide and the next slide. We achieve this by storing our elements in variables.
+    var currentItem = $('li.active');
+    //Position based selectors:
+    // first() https://api.jquery.com/first/
+    // last() https://api.jquery.com/last/
+    // prev() Documentation https://api.jquery.com/prev/
+    // next() Documentation https://api.jquery.com/next/
+    var previousItem = $('li.active').prev();
+
+    currentItem.toggleClass('active');
+
+    previousItem.toggleClass('active');
+
+    // Conditional eval.
+    if($('li').last().hasClass('active')) {
+      $('#next').hide();
+      $('#finish').show();
+    } else {
+      $('#next').show();
+    }
+    // Conditional eval.
+    if($('li').first().hasClass('active')) {
+      $('#previous').hide();
+    } else {
+      $('#previous').show();
+    }
 
   });
 
