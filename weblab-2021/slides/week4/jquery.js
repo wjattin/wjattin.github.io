b88c535c3c81f7e52c02c0d880110202ec156eed
William Jattin <wjattin@miami.edu>
2021-02-18 18:30:20 -0500

    asdsasd
    dasdsafd

---
+++ weblab-2021/slides/week4/jquery.js
@@ -0,0 +1,9 @@
+/*
+<script
+			  src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+			  integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
+			  crossorigin="anonymous"></script>
+*/
+$(function(){
+  $('.map').append('<h1>Welcome to the jungle</html>');
+});
